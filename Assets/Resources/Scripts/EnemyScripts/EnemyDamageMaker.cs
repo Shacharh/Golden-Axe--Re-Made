07ad41460118ec71f62878fe75f47c85a58b66ff
maxgol0 <maxgolberg2795@gmail.com>
2025-08-19 17:29:03 +0300

    have put the enemy scripts on the enemy and set all the relevant parameters in the inspector
    still need to fix animator

--- Assets/Resources/Scripts/EnemyScripts/EnemyDamageMaker.cs
+++ Assets/Resources/Scripts/EnemyScripts/EnemyDamageMaker.cs
@@ -4,7 +4,27 @@ public class EnemyDamageMaker : MonoBehaviour
 {
     public float enemyDamage = 20f;
     private Health playerHealth;
+    private Collider weaponCollider;
+    [SerializeField] private Animator enemyAnimator;
 
+    private void Awake()
+    {
+        enemyAnimator = GetComponent<Animator>();
+        weaponCollider = GetComponent<Collider>();
+    }
+
+    private void Update()
+    {
+        if(enemyAnimator.GetBool("isAttacking"))
+        {
+            weaponCollider.enabled = true;
+        }
+
+        else
+        {
+            weaponCollider.enabled = false;
+        }
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
