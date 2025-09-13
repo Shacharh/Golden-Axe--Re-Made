e22d6f93a3b8b9335294014ee4045038dd41c81f
Shachar <Hezkia.shachar@gmail.com>
2025-09-13 14:41:46 +0300

    started work on dust particle system references

---
+++ Assets/Resources/Scripts/PotionScripts/PotionPickUp.cs
@@ -0,0 +1,59 @@
+using System;
+using UnityEngine;
+
+public class PotionPickUp : MonoBehaviour
+{
+  /* 
+    Health _health;
+    [SerializeField] enum PotionType
+    {
+    Mana, HP
+    }
+    [SerializeField] int HPamount;
+    [SerializeField] int Manaamount;
+
+    private Collider trigger;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //identify if Player entered collider 
+        if(other.CompareTag("Player"))
+        {
+            // if HP/mana, depending on enum bar is full, return
+            switch (PotionType)
+            {
+                case PotionType.HP:
+                    if (_health.health >= _health.maxHealth)
+                        return;
+                    break;
+                case PotionType.Mana:
+                    //
+                        return;
+                    break;
+            }
+            
+
+            
+             // based on PotionType, figure out if need to add either Health, or mana to the player
+             switch (PotionType)
+             {
+                 case PotionType.HP:
+                     _health.AddHealth(HPamount);
+                     break;
+                 case PotionType.Mana:
+                     //
+                     break;
+             }
+             
+        
+        }
+       
+            
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }*/
+}
