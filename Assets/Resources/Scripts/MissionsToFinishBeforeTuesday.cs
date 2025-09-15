e5a7f3b34eff653f9ae09b4a4ef4995e3bb2855b
maxgol0 <maxgolberg2795@gmail.com>
2025-09-15 19:35:34 +0300

    updated my lost of missions. wrote it as a script file.

--- Assets/Resources/Scripts/MissionsToFinishBeforeTuesday.cs
+++ Assets/Resources/Scripts/MissionsToFinishBeforeTuesday.cs
@@ -2,9 +2,11 @@ using UnityEngine;
 
 public class MissionsToFinishBeforeTuesday : MonoBehaviour
 {
-    //enemies should recieve damage
+    //enemies should recieve damage V
     //enemies should spawn in the correct spawn pont in the wase system V
     //maybe nerf the enemies a little V
     // no more than 3 enemies on player at once
+    // small delay between waves
+    // door should open only after finishing last wave
 
 }
